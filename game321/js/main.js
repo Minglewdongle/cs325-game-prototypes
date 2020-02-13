439c9364f6e8208cea1fc24497b887a258545663
Vincent Chen <javavchen@gmail.com>
2020-02-13 06:22:33 -0500

    enter

--- game321/js/main.js
+++ game321/js/main.js
@@ -12,16 +12,43 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
+
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'marsh', 'assets/Marsh.png' );
+        game.load.image( 'sunrise', 'assets/sunset.png' );
+        game.load.image( 'ice', 'assets/ice.png' );
+        game.load.image( 'fire', 'assets/fire.gif');
+        game.load.image( 'water', 'assets/tex_Water.jpg');
     }
     
     var bouncy;
     
+    var player;
+    var input;
+    var fire;
+
+    var winText;
     function create() {
+
+        room=game.add.tileSprite(0,0,800,600,'sunrise');
+        player=game.add.sprite(game.world.centerX-400,game.world.centerY=+200,'marsh');
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        input=game.input.keyboard.createCursorKeys();
+        player.body.collideWorldBounds=true;
+        fire=game.add.sprite(game.world.centerX-100,game.world.centerY=+200,'fire');
+        ice=game.add.sprite(game.world.centerX -200,game.world.centerY=+200,'ice');
+        water=game.add.sprite(game.world.centerX ,game.world.centerY=+200,'water');
+        
+
+
+        winText= game.add.text(game.world.centerX,game.world.centerY, 'You stopped the fire!', font: "30px Verdana", fill: "#9999ff", align: "center");
+
+
+
+
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'marsh' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
@@ -45,5 +72,7 @@ window.onload = function() {
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
         bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+    
+    
     }
 };
